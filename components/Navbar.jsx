676a5515bf9eb5bacbb62939fe200bdf4d53433f
Arpit Kandwal <arpit.kandwal2004@gmail.com>
2023-09-13 17:35:32 +0530

    n

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
 import {PiShoppingCart} from 'react-icons/pi';
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href="/">Merch</Link>
+        <Link href="/">AnimeMerch</Link>
       </p>
-      <button type='button' className='cart-icon'>
+      <button type='button' className='cart-icon' onClick={() => {
+        setShowCart(true)
+      }}>
         <PiShoppingCart />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
       
+      { showCart && <Cart /> }
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import React  from 'react';
+import { Toaster } from 'react-hot-toast';
 import '../styles/globals.css';
 import { Layout } from '../components';
+import {StateContext} from '../context/StateContext';
 
 function App({ Component, pageProps }) {
   return (
+    <StateContext>
     <Layout>
+      <Toaster />
      <Component {...pageProps} />
      </Layout>
+     </StateContext>
   )
 }
 
